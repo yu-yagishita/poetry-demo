3d446deb8ebc04a5666785c87a7df9ce48147146
yu-yagishita <yu.yagishita1995@gmail.com>
2020-10-29 21:54:18 +0900

    testができるようにした

---
+++ poetry_demo/test.py
@@ -0,0 +1,4 @@
+def example(num_a,num_b,num_c,num_d,num_e,num_f,num_g,num_h):
+    print(num_a+num_b+num_c+num_d+num_e+num_f+num_g+num_h)
+num_a,num_b,num_c,num_d,num_e,num_f,num_g,num_h = 1,   2,  3,  4, 5,  6,  7,   8
+example(   num_a,     num_b,      num_c, num_d,num_e,   num_f,  num_g,   num_h  )
